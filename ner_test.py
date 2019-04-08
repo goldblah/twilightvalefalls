9fd7860f79b8beec83b631b8dca3d625d4819b06
hayleygoldblatt <hayley.goldblatt@enmu.edu>
2019-04-08 13:01:07 -0400

    Merge remote-tracking branch 'origin/master'

--- ner_test.py
+++ ner_test.py
@@ -1,11 +1,22 @@
 import nltk
 import pandas
+import re
+#import pymagic
+import sys
 
 dir = "/volumes/Hayley's Drive/PycharmProjects/twilightvalefalls/gravityfalls/"
 
 file = dir + 'A_Tale_of_Two_Stans.txt'
 
 with open(file, 'r') as myfile:
-    story = myfile.read()
+    story = myfile.read().replace('\n\n','\n')
 
-print(story)
+raw_story = r'' + story
+
+matches = re.finditer(r'(([\w ]*\:\s)([\w \.\,\'\-\?\!\(\)\:\"\;]*)\n)', raw_story)
+
+print(matches[0][0])
+
+
+
+
